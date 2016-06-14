edb4aefaa817b2eabd65183be8873df58862afcf
rurik <urrast@yandex.ru>
2016-06-14 15:55:05 +0300

    Hadoop4_1__4

--- src/main/java/Hadoop4_1__4.java
+++ src/main/java/Hadoop4_1__4.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * Created by rurik on 14.06.2016.
  */
@@ -5,6 +9,17 @@ public class Hadoop4_1__4 {
 
 
     public static void main(String[] args) {
-
+        Scanner reader = new Scanner(System.in);
+        String line;
+        while (reader.hasNext()) {
+            Map<String, Integer> countMap = new HashMap<>();
+            line = reader.nextLine();
+            for (String term : line.split(" ")) {
+                countMap.put(term, countMap.containsKey(term) ? countMap.get(term) + 1 : 1);
+            }
+            for (Map.Entry<String, Integer> pair : countMap.entrySet()) {
+                System.out.println(pair.getKey() + "\t" + pair.getValue());
+            }
+        }
     }
 }
