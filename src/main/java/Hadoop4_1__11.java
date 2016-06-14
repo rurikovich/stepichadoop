631f176cdcee8ca0d0f57a8962d7571e23116b86
rurik <urrast@yandex.ru>
2016-06-14 17:55:13 +0300

    Hadoop4_1__11

---
+++ src/main/java/Hadoop4_1__11.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Created by rurik on 14.06.2016.
+ */
+public class Hadoop4_1__11 {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String line;
+
+        while (reader.hasNext()) {
+            line = reader.nextLine();
+            String[] keyValue = line.split("\t");
+            String fg = keyValue[0];
+            String g = fg.split(",")[1];
+            System.out.println(g + "\t" + 1);
+        }
+    }
+
+
+}
