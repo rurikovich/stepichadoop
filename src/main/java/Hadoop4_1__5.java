86acdf3b5d7f81f4fc605ec1782fc442db7f1b00
rurik <urrast@yandex.ru>
2016-06-14 15:56:49 +0300

    Hadoop4_1__5

---
+++ src/main/java/Hadoop4_1__5.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Created by rurik on 14.06.2016.
+ */
+public class Hadoop4_1__5 {
+
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String line;
+        Map<String, Integer> countMap = new HashMap<>();
+        while (reader.hasNext()) {
+            line = reader.nextLine();
+            for (String term : line.split(" ")) {
+                countMap.put(term, countMap.containsKey(term) ? countMap.get(term) + 1 : 1);
+            }
+        }
+        for (Map.Entry<String, Integer> pair : countMap.entrySet()) {
+            System.out.println(pair.getKey() + "\t" + pair.getValue());
+        }
+    }
+}
