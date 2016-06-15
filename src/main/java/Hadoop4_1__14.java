41bc308e21e1916d4e87be9f0836842feb051ce1
rurik <urrast@yandex.ru>
2016-06-15 12:24:42 +0300

    Hadoop4_1__14

---
+++ src/main/java/Hadoop4_1__14.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Created by rurik on 14.06.2016.
+ */
+public class Hadoop4_1__14 {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String line;
+
+        while (reader.hasNext()) {
+            line = reader.nextLine();
+            String[] items = line.split(" ");
+            for (String a : items) {
+                for (String b : items) {
+                    if(!a.equals(b)){
+                        System.out.println(a + "," + b + "\t" + 1);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
