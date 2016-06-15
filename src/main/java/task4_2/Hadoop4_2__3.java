fcdfc1a9c09c5fee2de1abfcfad8fb9075b8696d
rurik <urrast@yandex.ru>
2016-06-15 17:53:35 +0300

    Hadoop4_2__3

---
+++ src/main/java/task4_2/Hadoop4_2__3.java
@@ -0,0 +1,21 @@
+package task4_2;
+
+import java.util.Scanner;
+
+/**
+ * Created by rurik on 14.06.2016.
+ */
+public class Hadoop4_2__3 {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String line;
+
+        while (reader.hasNext()) {
+            line = reader.nextLine();
+            String[] items = line.split("\t");
+            System.out.println(items[2]);
+        }
+    }
+
+}
