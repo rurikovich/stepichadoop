7b5774a082f41c6e97f7bc7444999fe8468b7161
rurik <urrast@yandex.ru>
2016-06-23 12:38:33 +0300

    Hadoop5_3__9

---
+++ src/main/java/task5_3/Hadoop5_3__9.java
@@ -0,0 +1,61 @@
+package task5_3;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * Created by rurik on 14.06.2016.
+ */
+public class Hadoop5_3__9 {
+
+    private static final String TAB = "\t";
+    public static final Integer INIT_VALUE = Integer.MAX_VALUE / 2;
+
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("#.###");
+        df.setRoundingMode(RoundingMode.FLOOR);
+
+        Scanner reader = new Scanner(System.in);
+        String prevKey = "";
+
+
+        Double sumWeight = 0d;
+        String arrToPrint = "";
+
+        while (reader.hasNext()) {
+//        for (int ii = 0; ii < 14; ii++) {
+            String line = reader.nextLine();
+            String[] data = line.split(TAB);
+
+            String key = data[0];
+            String weight = data[1];
+            String vertexes = data[2].replace("{", "").replace("}", "");
+
+
+            if (!(prevKey.equals("") || key.equals(prevKey))) {
+                System.out.println(prevKey + TAB + formatTo5Length(df.format(sumWeight)) + TAB + "{" + arrToPrint + "}");
+                sumWeight = 0d;
+                arrToPrint = "";
+            }
+            if (vertexes.isEmpty()) {
+                sumWeight += Double.parseDouble(weight);
+            } else {
+                arrToPrint = vertexes;
+            }
+            prevKey = key;
+        }
+        System.out.println(prevKey + TAB + formatTo5Length(df.format(sumWeight)) + TAB + "{" + arrToPrint + "}");
+
+    }
+
+    private static String formatTo5Length(String s) {
+        String res=s;
+        int n = 5 - s.length();
+        for (int i = 0; i < n; i++) {
+            res+="0";
+        }
+        return res;
+    }
+
+}
