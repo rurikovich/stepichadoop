4196e58d4fe28eb6d38255b1e23fdb9c59d64a83
rurik <urrast@yandex.ru>
2016-08-04 16:55:13 +0300

    CsAlgo1_1__4

---
+++ src/main/java/cs_algo_theory_and_practice_methods/task1_1/CsAlgo1_1__4.java
@@ -0,0 +1,20 @@
+package cs_algo_theory_and_practice_methods.task1_1;
+
+import java.util.Scanner;
+
+/**
+ * Created by rurik on 14.06.2016.
+ */
+public class CsAlgo1_1__4 {
+
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        int a = reader.nextInt();
+        int b = reader.nextInt();
+        System.out.println(a+b);
+
+
+    }
+}
