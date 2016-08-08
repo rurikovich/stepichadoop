8207ae4d6f11e716d3c462da2bd7c62bddd453c2
rurik <urrast@yandex.ru>
2016-08-08 12:30:51 +0300

    CsAlgo2_3__5

---
+++ src/main/java/cs_algo_theory_and_practice_methods/task2_3/task2_2/CsAlgo2_3__5.java
@@ -0,0 +1,32 @@
+package cs_algo_theory_and_practice_methods.task2_3.task2_2;
+
+import java.util.Scanner;
+
+/**
+ * Created by rurik on 14.06.2016.
+ */
+public class CsAlgo2_3__5 {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int a = reader.nextInt();
+        int b = reader.nextInt();
+        System.out.println(process(a, b));
+    }
+
+    public static int process(int a, int b) {
+        if (a == 0) {
+            return b;
+        } else if (b == 0) {
+            return a;
+        } else if (a >= b) {
+            return process(a % b, b);
+        } else if (b >= a) {
+            return process(a, b % a);
+        } else {
+            return 1;
+        }
+    }
+
+
+}
---
+++ src/test/groovy/cs_algo_theory_and_practice_methods/task2_3/CsAlgo2_3__5Spec.groovy
@@ -0,0 +1,26 @@
+package cs_algo_theory_and_practice_methods.task2_2
+
+import cs_algo_theory_and_practice_methods.task2_3.task2_2.CsAlgo2_3__5
+import spock.lang.Specification
+
+
+/**
+ * Created by rurik on 01.02.15.
+ */
+
+class CsAlgo2_3__5Spec extends Specification {
+
+//    1≤n≤10^18  и 2≤ m ≤10^5
+    def "tests works fine"() {
+        expect:
+        CsAlgo2_3__5.process(a, b) == res
+
+        where:
+        a        | b        || res
+        18       | 35       || 1
+        14159572 | 63967072 || 4
+
+
+    }
+
+}
