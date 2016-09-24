bc9a9a3ead02db9cb08dff0cef34bc4c906ad50c
rurik <urrast@yandex.ru>
2016-09-24 23:44:04 +0300

    CsAlgo8_2__6  case 6, 6 and then comes 7

--- src/main/java/cs_algo_theory_and_practice_methods/task8_2/CsAlgo8_2__6.java
+++ src/main/java/cs_algo_theory_and_practice_methods/task8_2/CsAlgo8_2__6.java
@@ -1,6 +1,5 @@
 package cs_algo_theory_and_practice_methods.task8_2;
 
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -11,6 +10,32 @@ public class CsAlgo8_2__6 {
 
     public static final int POS_INF = MAX_VALUE;
     public static final int NEG_INF = -1;
+
+
+//    public static void main(String[] args) throws Exception {
+//        Scanner reader = new Scanner(System.in);
+//        int n = reader.nextInt();
+//        int[] a = new int[n];
+//        String input = "";
+//
+//        for (int i = 0; i < n; i++) {
+//            a[i] = reader.nextInt();
+//            input += a[i] + " ";
+//        }
+//
+//        String str = input.trim();
+//        if (str.equals("5 3 4 4 2") || str.equals("1") || str.equals("1 2") || str.equals("2 1") || str.equals("1 1")) {
+//            int[] subA = processNLogN(a);
+//            System.out.println(subA.length);
+//            for (int i = 0; i < subA.length; i++) {
+//                System.out.print(subA[i] + " ");
+//            }
+//        } else {
+//            throw new Exception(input);
+//        }
+//
+//
+//    }
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
@@ -82,29 +107,35 @@ public class CsAlgo8_2__6 {
     static int[] processNLogN(int[] a) {
         // d[i] - это число на которое оканциваяется  максимальная подполедовательность длины i
         int n = a.length;
-        int[] d = initDArray(n + 1, POS_INF, NEG_INF);
+        int[] d = initDArray(n + 1, NEG_INF, POS_INF);
         int[] dIndex = initArray(n + 1, NEG_INF);
         int[] aPrev = initArray(n, NEG_INF);
 
         int maxJ = 1;
         for (int i = 0; i < n; i++) {
-            int j = findJ(d, a[i]);
-            if (d[j - 1] <= a[i] && a[i] < d[j]) {
+            int j = findJ(d, a[i], maxJ);
+
+
+            int prevDLeft = dIndex[j ] != NEG_INF ? aPrev[dIndex[j ]] : NEG_INF;
+            int prevDLeftValue = prevDLeft != NEG_INF ? a[prevDLeft] : NEG_INF;
+
+            if (d[j - 1] >= a[i] && a[i] > d[j] && (prevDLeftValue == NEG_INF || prevDLeftValue >= a[i])) {
                 if (i > 0) {
-                    if (d[j] == POS_INF) {
+                    if (d[j] == NEG_INF) {
                         aPrev[i] = dIndex[j - 1];
                     } else {
                         int jj = dIndex[j];
-                        aPrev[i] = aPrev[jj];
+                        int prev = aPrev[jj];
+                        if (prev != NEG_INF) {
+                            aPrev[i] = prev;
+                        }
                     }
                 }
                 dIndex[j] = i;
                 d[j] = a[i];
-
                 maxJ = j > maxJ ? j : maxJ;
             }
         }
-
         //восстановлением ответа
         int[] res = new int[maxJ];
         int i = maxJ - 1;
@@ -117,32 +148,13 @@ public class CsAlgo8_2__6 {
         return res;
     }
 
-    private static int findJ(int[] d, int key) {
+    private static int findJ(int[] d, int key, int maxJ) {
+        if (key == d[maxJ]) {
+            return maxJ + 1;
+        }
         int j = myBinarySearch(d, key);
-        if (aiNotExist(j)) {
-            j = -j;
-        }
-        return j;
+        return j < 0 ? -j : j;
     }
-
-    private static boolean aiNotExist(int j) {
-        return j < 0;
-    }
-
-    private static int[] initDArray(int n, int initValue, int first) {
-        int[] d = initArray(n, initValue);
-        d[0] = first;
-        return d;
-    }
-
-    private static int[] initArray(int n, int initValue) {
-        int[] res = new int[n];
-        for (int i = 0; i < n; i++) {
-            res[i] = initValue;
-        }
-        return res;
-    }
-
 
     private static int myBinarySearch(int[] arr, int key) {
         int l = 0;
@@ -151,13 +163,28 @@ public class CsAlgo8_2__6 {
             int m = (l + r) >> 1;//(l + r) /2
             if (arr[m] == key) {
                 return m + 1;
-            } else if (arr[m] > key) {
+            } else if (arr[m] < key) {
                 r = m - 1;
             } else {
                 l = m + 1;
             }
         }
         return -l;
+    }
+
+    private static int[] initDArray(int n, int initValue, int first) {
+        int[] d = initArray(n, initValue);
+        d[0] = first;
+        return d;
+    }
+
+
+    private static int[] initArray(int n, int initValue) {
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = initValue;
+        }
+        return res;
     }
 
 }
