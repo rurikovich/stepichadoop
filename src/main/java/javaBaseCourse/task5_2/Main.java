78de28b7dd392130d630bdd596b605f594a40cc0
rurik <urrast@yandex.ru>
2017-01-09 10:43:13 +0300

    task5_2

--- src/main/java/javaBaseCourse/task5_2/Main.java
+++ src/main/java/javaBaseCourse/task5_2/Main.java
@@ -1,5 +1,6 @@
 package javaBaseCourse.task5_2;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,12 +13,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
 //        byte r = 0x13;
 //        byte n = 0x10;
+
+
+//        byte[] arr = {65, 66, 13, 13, 10, 10, 13, 67, 13, 13};
+//        ByteArrayInputStream inputStream = new ByteArrayInputStream(arr);
+//        System.setIn(inputStream);
+
         int r = 13;
         int n = 10;
 
         try (InputStream in = System.in;
              OutputStream out = System.out) {
             int prev = in.read();
+            if(prev==-1){
+                return;
+            }
             int cur = in.read();
 
             while (cur > 0) {
@@ -27,6 +37,7 @@ public class Main {
                 prev = cur;
                 cur = in.read();
             }
+            out.write(prev);
         }
         System.out.flush();
     }
