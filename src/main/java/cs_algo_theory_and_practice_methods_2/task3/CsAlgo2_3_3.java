0f8d2286ac6848f4c4fd4fc11002f25f6f2ab4e7
rurik <urrast@yandex.ru>
2017-05-07 11:02:13 +0300

    CsAlgo2_3_3 works for test <36

--- src/main/java/cs_algo_theory_and_practice_methods_2/task3/CsAlgo2_3_3.java
+++ src/main/java/cs_algo_theory_and_practice_methods_2/task3/CsAlgo2_3_3.java
@@ -9,6 +9,9 @@ import java.util.Scanner;
  */
 public class CsAlgo2_3_3 {
 
+    static int p = 1_000_000_007;
+    static int x = 263;
+
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         String pattern = reader.next();
@@ -23,14 +26,13 @@ public class CsAlgo2_3_3 {
     }
 
     static List<Integer> findPatternIndexes(String pattern, String text) {
-        int p = 1_000_000_007;
-        int x = 263;
+
 
         List<Integer> indexes = new ArrayList<>();
 
         int patternHash = h(pattern, p, x);
         int pLength = pattern.length();
-        long x_pow_n_mod_p = x_pow_n_mod_p(1, x, pLength, p);
+        long x_pow_n_mod_p = x_pow_n_mod_p(1, x, pLength - 1, p);
 
         int startIndex = text.length() - pLength;
         String lastStr = text.substring(startIndex, text.length());
@@ -46,14 +48,11 @@ public class CsAlgo2_3_3 {
             int lastCharCode = (int) text.charAt(i + pLength);
             int firstCharCode = (int) text.charAt(i);
 
-//            long hash = (((prevHash - (lastCharCode * x_pow_n_mod_p) % p) % p * x) % p + firstCharCode) % p;
+            long hash = getNextHash(prevHash, x, p, x_pow_n_mod_p, lastCharCode, firstCharCode);
 
+//            long realHash = h(text.substring(i, i + pLength), p, x);
 
-            long hash = ((prevHash - lastCharCode * x_pow_n_mod_p) * x + firstCharCode) % p;
-
-            long realHash = h(text.substring(i, i + pLength), p, x);
-
-            if (realHash == patternHash && text.substring(i, i + pLength).equals(pattern)) {
+            if (hash == patternHash && text.substring(i, i + pLength).equals(pattern)) {
                 indexes.add(i);
             }
             prevHash = hash;
@@ -61,6 +60,10 @@ public class CsAlgo2_3_3 {
 
 
         return indexes;
+    }
+
+    static long getNextHash(long prevHash, int x, int p, long x_pow_n_mod_p, int lastCharCode, int firstCharCode) {
+        return ((((prevHash - (lastCharCode * x_pow_n_mod_p) % p) + p) % p * x) % p + firstCharCode) % p;
     }
 
 
@@ -77,7 +80,7 @@ public class CsAlgo2_3_3 {
         return (int) res;
     }
 
-    private static long x_pow_n_mod_p(int startValue, int x, int n, int p) {
+    static long x_pow_n_mod_p(int startValue, int x, int n, int p) {
         long x_mod_p = x % p;
         long pow_mod_p = startValue;
         for (int j = 0; j < n; j++) {
--- src/test/java/cs_algo_theory_and_practice_methods_2/task3/CsAlgo2_3_3Test.java
+++ src/test/java/cs_algo_theory_and_practice_methods_2/task3/CsAlgo2_3_3Test.java
@@ -2,6 +2,7 @@ package cs_algo_theory_and_practice_methods_2.task3;
 
 import org.junit.Test;
 
+import static cs_algo_theory_and_practice_methods_2.task3.CsAlgo2_3_3.*;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
@@ -9,13 +10,38 @@ import static org.junit.Assert.assertEquals;
  * Created by User on 06.05.2017.
  */
 public class CsAlgo2_3_3Test {
+
     @Test
     public void findPatternIndexes() throws Exception {
-        assertEquals(asList(4,0), CsAlgo2_3_3.findPatternIndexes("aba", "abacaba"));
+        assertEquals(asList(4, 0), CsAlgo2_3_3.findPatternIndexes("aba", "abacaba"));
 
-//        assertEquals(asList(4), CsAlgo2_3_3.findPatternIndexes("Test", "testTesttesT"));
-//
-//        assertEquals(asList(3, 2, 1), CsAlgo2_3_3.findPatternIndexes("aaaaa", "baaaaaaa"));
+        assertEquals(asList(4), CsAlgo2_3_3.findPatternIndexes("Test", "testTesttesT"));
+
+        assertEquals(asList(3, 2, 1), CsAlgo2_3_3.findPatternIndexes("aaaaa", "baaaaaaa"));
+    }
+
+    @Test
+    public void getNextHashTest() throws Exception {
+        int p = 19;
+        int x = 7;
+
+        String str = "abcd";
+        int n = 2 ;
+        long x_pow_n_mod_p = x_pow_n_mod_p(1, x, n, p);
+
+
+        long hash = h(str.substring(1, str.length()), p, x);
+
+
+        int lastCharCode = (int) str.charAt(str.length() - 1);
+        int firstCharCode = (int) str.charAt(0);
+        long nextHash = getNextHash(hash, x, p, x_pow_n_mod_p, lastCharCode, firstCharCode);
+
+        long realNextHash = h(str.substring(0, str.length() - 1), p, x);
+
+
+        assertEquals(realNextHash, nextHash);
+
     }
 
 }
