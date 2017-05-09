0e9fa4960a4e0eb37568c10e6a375e226002ec1e
rurik <urrast@yandex.ru>
2017-05-09 18:47:58 +0300

    CsAlgo2_4_1

--- src/main/java/cs_algo_theory_and_practice_methods_2/task4/CsAlgo2_4_1.java
+++ src/main/java/cs_algo_theory_and_practice_methods_2/task4/CsAlgo2_4_1.java
@@ -1,6 +1,7 @@
 package cs_algo_theory_and_practice_methods_2.task4;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -20,45 +21,90 @@ public class CsAlgo2_4_1 {
             tree.add(new TreeItem(key, left, right));
         }
 
-         inOrderTreeSearch(tree, 0);
-         preOrderTreeSearch(tree, 0);
-         postOrderTreeSearch(tree, 0);
+        inOrderTreeSearchSystemOut(tree, 0);
+        preOrderTreeSearchSystemOut(tree, 0);
+        postOrderTreeSearchSystemOut(tree, 0);
+    }
 
+    static List<Integer> inOrderTreeSearch(ArrayList<TreeItem> tree, int top) {
+        List<Integer> res = new ArrayList<>();
+        TreeItem item = tree.get(top);
+        if (item.left != -1) {
+            List<Integer> leftKeys = inOrderTreeSearch(tree, item.left);
+            res.addAll(leftKeys);
+        }
+        res.add(item.key);
+        if (item.right != -1) {
+            List<Integer> rightKeys = inOrderTreeSearch(tree, item.right);
+            res.addAll(rightKeys);
+        }
+        return res;
+    }
+
+    static List<Integer> preOrderTreeSearch(ArrayList<TreeItem> tree, int top) {
+        List<Integer> res = new ArrayList<>();
+        TreeItem item = tree.get(top);
+
+        res.add(item.key);
+
+        if (item.left != -1) {
+            List<Integer> leftKeys = preOrderTreeSearch(tree, item.left);
+            res.addAll(leftKeys);
+        }
+        if (item.right != -1) {
+            List<Integer> rightKeys = preOrderTreeSearch(tree, item.right);
+            res.addAll(rightKeys);
+        }
+        return res;
+    }
+
+    static List<Integer> postOrderTreeSearch(ArrayList<TreeItem> tree, int top) {
+        List<Integer> res = new ArrayList<>();
+        TreeItem item = tree.get(top);
+        if (item.left != -1) {
+            List<Integer> leftKeys = postOrderTreeSearch(tree, item.left);
+            res.addAll(leftKeys);
+        }
+        if (item.right != -1) {
+            List<Integer> rightKeys = postOrderTreeSearch(tree, item.right);
+            res.addAll(rightKeys);
+        }
+        res.add(item.key);
+        return res;
+    }
+
+    static void inOrderTreeSearchSystemOut(ArrayList<TreeItem> tree, int top) {
+        TreeItem item = tree.get(top);
+        if (item.left != -1) {
+            inOrderTreeSearchSystemOut(tree, item.left);
+        }
+        System.out.print(item.key + " ");
+        if (item.right != -1) {
+            inOrderTreeSearchSystemOut(tree, item.right);
+        }
+    }
+
+    static void preOrderTreeSearchSystemOut(ArrayList<TreeItem> tree, int top) {
+        TreeItem item = tree.get(top);
+        System.out.print(item.key + " ");
+        if (item.left != -1) {
+            preOrderTreeSearchSystemOut(tree, item.left);
+        }
+        if (item.right != -1) {
+            preOrderTreeSearchSystemOut(tree, item.right);
+        }
 
     }
 
-    static void inOrderTreeSearch(ArrayList<TreeItem> tree, int top) {
+    static void postOrderTreeSearchSystemOut(ArrayList<TreeItem> tree, int top) {
         TreeItem item = tree.get(top);
         if (item.left != -1) {
-            inOrderTreeSearch(tree, item.left);
-        }
-        System.out.print(item.key+" ");
-        if (item.right != -1) {
-             inOrderTreeSearch(tree, item.right);
-        }
-    }
-
-    static void preOrderTreeSearch(ArrayList<TreeItem> tree, int top) {
-        TreeItem item = tree.get(top);
-        System.out.print(item.key+" ");
-        if (item.left != -1) {
-            preOrderTreeSearch(tree, item.left);
+            postOrderTreeSearchSystemOut(tree, item.left);
         }
         if (item.right != -1) {
-            preOrderTreeSearch(tree, item.right);
+            postOrderTreeSearchSystemOut(tree, item.right);
         }
-
-    }
-
-    static void postOrderTreeSearch(ArrayList<TreeItem> tree, int top) {
-        TreeItem item = tree.get(top);
-        if (item.left != -1) {
-             postOrderTreeSearch(tree, item.left);
-        }
-        if (item.right != -1) {
-             postOrderTreeSearch(tree, item.right);
-        }
-        System.out.print(item.key+" ");
+        System.out.print(item.key + " ");
     }
 
 
