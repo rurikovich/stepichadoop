435c1166796a69118518ad5d2738b6695909bcd8
rurik <urrast@yandex.ru>
2017-05-27 16:45:51 +0400

    ziq zaq methods implementation

--- src/main/java/cs_algo_theory_and_practice_methods_2/task4/SplayTreeTask.java
+++ src/main/java/cs_algo_theory_and_practice_methods_2/task4/SplayTreeTask.java
@@ -81,61 +81,139 @@ public class SplayTreeTask {
 
 
         private void splay(SplayTreeItem item) {
-            if (item.parent == -1) {
-                return;
-            }
+            while (item.parent != -1) {
+                SplayTreeItem parent = items.get(item.parent);
+                if (parent.parent != -1) {
+                    SplayTreeItem grandFather = items.get(parent.parent);
 
-            SplayTreeItem parent = items.get(item.parent);
-            if (parent.parent != -1) {
-                SplayTreeItem grandFather = items.get(parent.parent);
-
-                if (item.isLeftChild() && parent.isLeftChild()) {
-                    ziqziq(item, parent, grandFather);
-                } else if (item.isLeftChild() && parent.isRightChild()) {
-                    ziqzaq(item, parent, grandFather);
-                } else if (item.isRightChild() && parent.isRightChild()) {
-                    zaqzaq(item, parent, grandFather);
-                } else if (item.isRightChild() && parent.isLeftChild()) {
-                    zaqziq(item, parent, grandFather);
+                    if (item.isLeftChild() && parent.isLeftChild()) {
+                        ziqziq(item, parent, grandFather);
+                    } else if (item.isLeftChild() && parent.isRightChild()) {
+                        ziqzaq(item, parent, grandFather);
+                    } else if (item.isRightChild() && parent.isRightChild()) {
+                        zaqzaq(item, parent, grandFather);
+                    } else if (item.isRightChild() && parent.isLeftChild()) {
+                        zaqziq(item, parent, grandFather);
+                    }
+                } else {
+                    if (item.isLeftChild()) {
+                        ziq(item, parent);
+                    } else if (item.isRightChild()) {
+                        zaq(item, parent);
+                    }
                 }
-            } else {
-                if (item.isLeftChild()) {
-                    ziq(item, parent);
-                } else if (item.isRightChild()) {
-                    zaq(item, parent);
-                }
-
-
             }
-
         }
 
-        private void ziqziq(SplayTreeItem splayTreeItem, SplayTreeItem parent, SplayTreeItem item) {
+        private void ziqziq(SplayTreeItem item, SplayTreeItem parent, SplayTreeItem grandFather) {
+            int grandFatherParentIndex = grandFather.parent;
+            ChildType grandFatherChildType = grandFather.childType;
 
+            updateLeftChild(grandFather, parent.right);
+            grandFather.parent = parent.index;
+
+            updateRightChild(parent, grandFather.index);
+            parent.parent = item.index;
+
+            updateRightChild(item, parent.index);
+            item.childType = grandFatherChildType;
+            item.parent = grandFatherParentIndex;
         }
 
-        private void ziqzaq(SplayTreeItem splayTreeItem, SplayTreeItem parent, SplayTreeItem item) {
 
+        private void ziqzaq(SplayTreeItem item, SplayTreeItem parent, SplayTreeItem grandFather) {
+            int grandFatherParentIndex = grandFather.parent;
+            ChildType grandFatherChildType = grandFather.childType;
+
+            updateRightChild(grandFather, item.left);
+            grandFather.parent = item.index;
+
+            updateLeftChild(parent, item.right);
+            parent.parent = item.index;
+
+            updateRightChild(item, parent.index);
+            updateLeftChild(item, grandFather.index);
+            item.parent = grandFatherParentIndex;
+            item.childType = grandFatherChildType;
         }
 
         private void zaqzaq(SplayTreeItem item, SplayTreeItem parent, SplayTreeItem grandFather) {
+            int grandFatherParentIndex = grandFather.parent;
+            ChildType grandFatherChildType = grandFather.childType;
 
+            updateRightChild(grandFather, parent.left);
+            grandFather.parent = parent.index;
+
+            updateRightChild(parent, item.left);
+            updateLeftChild(parent, grandFather.index);
+            parent.parent = item.index;
+
+            updateLeftChild(item, parent.index);
+            item.parent = grandFatherParentIndex;
+            item.childType = grandFatherChildType;
         }
 
         private void zaqziq(SplayTreeItem item, SplayTreeItem parent, SplayTreeItem grandFather) {
+            int grandFatherParentIndex = grandFather.parent;
+            ChildType grandFatherChildType = grandFather.childType;
+
+            updateLeftChild(grandFather, item.right);
+            grandFather.parent = item.index;
+
+            updateRightChild(parent, item.left);
+            parent.parent = item.index;
+
+            updateRightChild(item, grandFather.index);
+            updateLeftChild(item, parent.index);
+            item.parent = grandFatherParentIndex;
+            item.childType = grandFatherChildType;
 
         }
 
 
         private void ziq(SplayTreeItem item, SplayTreeItem parent) {
+            int parentParentIndex = parent.parent;
+            ChildType parentChildType = parent.childType;
 
+            updateLeftChild(parent, item.right);
+            parent.parent = item.index;
+
+            updateRightChild(item, parent.index);
+            item.parent = parentParentIndex;
+            item.childType = parentChildType;
         }
 
         private void zaq(SplayTreeItem item, SplayTreeItem parent) {
+            int parentParentIndex = parent.parent;
+            ChildType parentChildType = parent.childType;
 
+            updateRightChild(parent, item.left);
+            parent.parent = item.index;
+
+            updateLeftChild(item, parent.index);
+            item.parent = parentParentIndex;
+            item.childType = parentChildType;
         }
 
 
+        private void updateLeftChild(SplayTreeItem itemToUpdate, int newLeftChildIndex) {
+            itemToUpdate.left = newLeftChildIndex;
+            if (newLeftChildIndex != -1) {
+                SplayTreeItem item = items.get(newLeftChildIndex);
+                item.childType = LEFT;
+                item.parent = itemToUpdate.index;
+            }
+        }
+
+
+        private void updateRightChild(SplayTreeItem itemToUpdate, int newRightChildIndex) {
+            itemToUpdate.right = newRightChildIndex;
+            if (newRightChildIndex != -1) {
+                SplayTreeItem item = items.get(newRightChildIndex);
+                item.childType = RIGHT;
+                item.parent = itemToUpdate.index;
+            }
+        }
     }
 
 
