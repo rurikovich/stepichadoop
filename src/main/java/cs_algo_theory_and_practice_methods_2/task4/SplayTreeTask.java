56f5c107ce16fcc1851797cc87dbe13d1650c1a3
rurik <urrast@yandex.ru>
2017-05-27 15:36:43 +0400

    ziq zaq methods

--- src/main/java/cs_algo_theory_and_practice_methods_2/task4/SplayTreeTask.java
+++ src/main/java/cs_algo_theory_and_practice_methods_2/task4/SplayTreeTask.java
@@ -3,6 +3,10 @@ package cs_algo_theory_and_practice_methods_2.task4;
 import java.util.ArrayList;
 import java.util.List;
 
+import static cs_algo_theory_and_practice_methods_2.task4.SplayTreeTask.ChildType.LEFT;
+import static cs_algo_theory_and_practice_methods_2.task4.SplayTreeTask.ChildType.NOT_A_CHILD;
+import static cs_algo_theory_and_practice_methods_2.task4.SplayTreeTask.ChildType.RIGHT;
+
 /**
  * Created by rurik on 25/05/17.
  */
@@ -44,6 +48,11 @@ public class SplayTreeTask {
         }
 
         SplayTreeItem insert(int key, int index) {
+            if (items.isEmpty()) {
+                SplayTreeItem treeItem = new SplayTreeItem(key, -1, -1, -1, 0, NOT_A_CHILD);
+                items.add(treeItem);
+            }
+
             SplayTreeItem item = items.get(index);
             if (key == item.key) {
                 return item;
@@ -51,7 +60,7 @@ public class SplayTreeTask {
                 if (item.left != -1) {
                     return insert(key, item.left);
                 } else {
-                    SplayTreeItem treeItem = new SplayTreeItem(key, item.index, -1, -1, items.size());
+                    SplayTreeItem treeItem = new SplayTreeItem(key, item.index, -1, -1, items.size(), LEFT);
                     item.left = items.size();
                     items.add(treeItem);
                     return treeItem;
@@ -60,7 +69,7 @@ public class SplayTreeTask {
                 if (item.right != -1) {
                     return insert(key, item.right);
                 } else {
-                    SplayTreeItem treeItem = new SplayTreeItem(key, item.index, -1, -1, items.size());
+                    SplayTreeItem treeItem = new SplayTreeItem(key, item.index, -1, -1, items.size(), RIGHT);
                     item.right = items.size();
                     items.add(treeItem);
                     return treeItem;
@@ -80,19 +89,49 @@ public class SplayTreeTask {
             if (parent.parent != -1) {
                 SplayTreeItem grandFather = items.get(parent.parent);
 
-
-
+                if (item.isLeftChild() && parent.isLeftChild()) {
+                    ziqziq(item, parent, grandFather);
+                } else if (item.isLeftChild() && parent.isRightChild()) {
+                    ziqzaq(item, parent, grandFather);
+                } else if (item.isRightChild() && parent.isRightChild()) {
+                    zaqzaq(item, parent, grandFather);
+                } else if (item.isRightChild() && parent.isLeftChild()) {
+                    zaqziq(item, parent, grandFather);
+                }
             } else {
+                if (item.isLeftChild()) {
+                    ziq(item, parent);
+                } else if (item.isRightChild()) {
+                    zaq(item, parent);
+                }
+
 
             }
 
         }
 
-        private void ziqziq(SplayTreeItem item){
+        private void ziqziq(SplayTreeItem splayTreeItem, SplayTreeItem parent, SplayTreeItem item) {
 
         }
 
-        private void ziqzaq(SplayTreeItem item){
+        private void ziqzaq(SplayTreeItem splayTreeItem, SplayTreeItem parent, SplayTreeItem item) {
+
+        }
+
+        private void zaqzaq(SplayTreeItem item, SplayTreeItem parent, SplayTreeItem grandFather) {
+
+        }
+
+        private void zaqziq(SplayTreeItem item, SplayTreeItem parent, SplayTreeItem grandFather) {
+
+        }
+
+
+        private void ziq(SplayTreeItem item, SplayTreeItem parent) {
+
+        }
+
+        private void zaq(SplayTreeItem item, SplayTreeItem parent) {
 
         }
 
@@ -106,14 +145,32 @@ public class SplayTreeTask {
         int left;
         int right;
         int index;
+        ChildType childType;
 
-        public SplayTreeItem(int key, int parent, int left, int right, int index) {
+        public SplayTreeItem(int key, int parent, int left, int right, int index, ChildType childType) {
             this.key = key;
             this.parent = parent;
             this.left = left;
             this.right = right;
             this.index = index;
+            this.childType = childType;
         }
+
+        public boolean isLeftChild() {
+            return childType.equals(LEFT);
+        }
+
+
+        public boolean isRightChild() {
+            return childType.equals(RIGHT);
+        }
+
+    }
+
+    enum ChildType {
+        LEFT,
+        RIGHT,
+        NOT_A_CHILD
     }
 
 
